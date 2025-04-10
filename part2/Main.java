5a3bdf43b2398329c10e78e47e971a7239286b63
Giorgos Oulis <68658519+giorgoul@users.noreply.github.com>
2025-04-10 19:53:47 +0300

    part2: converting IR -> java, example 1 runs

--- part2/Main.java
+++ part2/Main.java
@@ -3,7 +3,9 @@ import java.io.*;
 
 class Main {
     public static void main(String[] argv) throws Exception{
-        ParserIR p = new ParserIR(new ScannerIR(new InputStreamReader(System.in)));
-        p.parse();
+        ParserIR p_ir = new ParserIR(new ScannerIR(new InputStreamReader(System.in)));
+        p_ir.parse();
+        ParserIRJava p_ir_java = new ParserIRJava(new ScannerIR(new FileReader("Translated.ir")));
+        p_ir_java.parse();
     }
 }
